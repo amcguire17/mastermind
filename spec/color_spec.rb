4d25c09b1fa358a37b85c0bb5352f88eff8fde21
Jacob Piland <piland.jacob@gmail.com>
2021-05-18 14:35:59 -0500

    add different color test

--- spec/color_spec.rb
+++ spec/color_spec.rb
@@ -14,4 +14,15 @@ RSpec.describe Color do
     expect(color.name).to eq("red")
     expect(color.abbr).to eq(:r)
   end
-end 
+
+  it "has different color" do
+    color1 = Color.new("red", :r)
+    color2 = Color.new("blue", :b)
+
+    expect(color1.name).to eq("red")
+    expect(color1.abbr).to eq(:r)
+    expect(color2.name).to eq("blue")
+    expect(color2.abbr).to eq(:b)
+  end 
+
+end
