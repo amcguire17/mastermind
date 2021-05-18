5632e66f5a0059ddda8b7b174d6cc28a72dff409
Amanda McGuire <aruwart001@gmail.com>
2021-05-18 14:01:49 -0600

    add new guess class and test file

---
+++ spec/guess_spec.rb
@@ -0,0 +1,12 @@
+require './lib/color'
+require './lib/guess'
+
+Rspec.describe Guess do
+  describe "exists" do
+    red = Color.new("red", :r)
+    blue = Color.new("blue", :b)
+    yellow = Color.new("yellow", :y)
+    green = Color.new("green", :g)
+    
+  end
+end
