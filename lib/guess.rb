4e907d9212ae202cb37d03182df1dcb22d40b719
Amanda McGuire <aruwart001@gmail.com>
2021-05-19 19:11:32 -0600

    update user input method and change abbr symbols to strings

--- lib/guess.rb
+++ lib/guess.rb
@@ -3,25 +3,29 @@ class Guess
 
   def initialize(colors)
     @colors = colors
+    @user_guess = []
+  end
+
+  def add_guess(guess)
+    @user_guess << guess
   end
 
   def secret_code
     @colors.sample(4)
   end
-
-  def user_input
-    loop do
-      @user_guess = gets.chomp.split("")
-      if @user_guess == secret_code.length && @user_guess.include?(@colors.uniq)
-        break
-      elsif @user_guess > secret_code.length
-        p "You're guess is too long. Try again."
-      elsif @user_guess < secret_code.length
-        p "You're guess is too short. Try again."
-      # else
-      #   p "User input invalid. Try again."
-      end
-    end
-    require 'pry'; binding.pry
-  end
 end
+
+# def user_input
+#   loop do
+#     @user_guess = gets.chomp.split("")
+#     if @user_guess == secret_code.length && @user_guess.include?(@colors.uniq)
+#       break
+#     elsif @user_guess > secret_code.length
+#       p "Your guess is too long. Try again."
+#     elsif @user_guess < secret_code.length
+#       p "Your guess is too short. Try again."
+#     else
+#       p "User input invalid. Try again."
+#     end
+#   end
+# end
--- spec/guess_spec.rb
+++ spec/guess_spec.rb
@@ -3,10 +3,10 @@ require './lib/guess'
 
 RSpec.describe Guess do
   before(:each) do
-    @red = Color.new("red", :r)
-    @blue = Color.new("blue", :b)
-    @yellow = Color.new("yellow", :y)
-    @green = Color.new("green", :g)
+    @red = Color.new("red", "r")
+    @blue = Color.new("blue", "b")
+    @yellow = Color.new("yellow", "y")
+    @green = Color.new("green", "g")
     @colors = [@red, @blue, @yellow, @green]
     @guess = Guess.new(@colors)
   end
@@ -23,6 +23,17 @@ RSpec.describe Guess do
 
   end
 
+  it "has no guesses" do
+
+    expect(@guess.user_guess).to eq([])
+
+  end
+
+  it "can add guesses" do
+
+    expect(@guess.add_guess(["r", "g", "b", "y"])).to eq(@guess.user_guess)
+  end
+
   it "can create secret code" do
 
     expect(@guess.secret_code).not_to eq(@colors)
@@ -30,21 +41,15 @@ RSpec.describe Guess do
   end
 
   it "can select from more colors" do
-    red2 = Color.new("red", :r)
-    blue2 = Color.new("blue", :b)
-    yellow2 = Color.new("yellow", :y)
-    green2 = Color.new("green", :g)
+    red2 = Color.new("red", "r")
+    blue2 = Color.new("blue", "b")
+    yellow2 = Color.new("yellow", "y")
+    green2 = Color.new("green", "g")
     colors = [@red, @blue, @yellow, @green, red2, blue2, yellow2, green2]
     guess = Guess.new(colors)
 
     expect(guess.secret_code).not_to eq(colors)
 
-  end
-
-  it "can take user input" do
-
-
-  end
   end
 
 end
